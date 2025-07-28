a38b1ca607b9be4957198ed44c5c20f856dee6ec
Teodoro Anello <giacomoeneldo@gmail.com>
2025-07-28 08:00:18 +0200

    DSL con notificaciones mail y slack

--- JenkinsWork/DSL/Ejemplo1.groovy
+++ JenkinsWork/DSL/Ejemplo1.groovy
@@ -17,4 +17,25 @@ job('ejemplo-dsl') {
   steps{
     shell('JenkinsWork/scripts/backupjenkinscontenedor.sh "$ruta" "$ejecutar" "$comando"')
   }
+  publishers {
+    mailer('gopycodewar@gmail.com',true,true)
+    slackNotifier{ 
+      notifyAborted(true)
+      notifyEveryFailure(true)
+      notifyNotBuilt(false)
+      notifyUnstable(false)
+      notifyBackToNormal(true)
+      notifySuccess(false)
+      notifyRepeatedFailure(false)
+      startNotification(false)
+      includeTestSummary(false)
+      includeCustomMessage(false)
+      customMessage(null)
+      sendAs(null)
+      commitInfoChoice('NONE')
+      teamDomain(null)
+      authToken(null)
+    }
+  }
+  
 }
