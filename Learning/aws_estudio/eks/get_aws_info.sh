f9f9f24d3a176d5a7613dac84c83e34e97a649ec
italianodevops <gopycodewar@gmail.com>
2026-04-05 18:14:31 +0200

    get pod info

---
+++ Learning/aws_estudio/eks/get_aws_info.sh
@@ -0,0 +1,7 @@
+NODE_PRIVATE_DNS=$(kubectl get pod <pod-name> -o=jsonpath='{.spec.nodeName}')
+SERVICE_NODE_PORT=$(kubectl get svc <service-name> -o=jsonpath='{.spec.ports[0].nodePort}')
+NODE_PUBLIC_IP=$(aws ec2 describe-instances  --region <region> --query "Reservations[0].Instances[0].PublicIpAddress" --filters "Name=private-dns-name,Values=$NODE_PRIVATE_DNS" --output text)
+SECURITY_GROUP_ID=$(aws ec2 describe-instances  --region <region> --query "Reservations[].Instances[].NetworkInterfaces[].Groups[].GroupId"  --filters "Name=private-dns-name,Values=$NODE_PRIVATE_DNS" --output text)
+
+
+aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP_ID --protocol "tcp" --port $SERVICE_NODE_PORT --cidr 0.0.0.0/0 --region eu-south-2
